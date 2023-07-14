949af3ca00b080ab53d9501d1a67d34fe5240685
Andrew Duffy <andreweduffy@gmail.com>
2023-07-13 21:23:36 -0400

    various fixes

--- src/CesiumView.tsx
+++ src/CesiumView.tsx
@@ -2,19 +2,18 @@ import * as Cesium from "cesium";
 import { useEffect, useRef } from "react";
 
 export default function CesiumView({
-    entities,
+  entities,
 }: {
-    entities: Array<Cesium.Entity>,
+  entities: Array<Cesium.Entity>;
 }) {
   const viewerNode = useRef<HTMLDivElement>(null);
   const viewer = useRef<Cesium.Viewer>();
 
   useEffect(() => {
-    console.log("effect1");
-    const osm = Cesium.ImageryLayer.fromProviderAsync(
+    const cartodb = Cesium.ImageryLayer.fromProviderAsync(
       Promise.resolve(
-        new Cesium.OpenStreetMapImageryProvider({
-          url: "https://a.tile.openstreetmap.org/",
+        new Cesium.UrlTemplateImageryProvider({
+          url: "http://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png",
         })
       ),
       {}
@@ -22,48 +21,40 @@ export default function CesiumView({
     viewer.current = new Cesium.Viewer(viewerNode.current!, {
       animation: false,
       fullscreenButton: false,
-      //vrButton: false,
+      vrButton: false,
       homeButton: false,
       navigationHelpButton: false,
       scene3DOnly: true,
       skyBox: false,
       skyAtmosphere: false,
       baseLayerPicker: false,
-      baseLayer: osm,
+      baseLayer: cartodb,
       timeline: false,
-      //infoBox: false,
       terrainShadows: Cesium.ShadowMode.ENABLED,
       useBrowserRecommendedResolution: true,
     });
     viewer.current.provider;
     viewer.current.cesiumWidget.creditContainer.remove();
 
-
     return () => {
       viewer.current?.destroy();
       viewer.current = undefined;
     };
-    // TODO(aduffy): Shouldn't this also depend on entities? We should probably do something to support those when they don't change either...but separately from the other stuff.
   }, [viewerNode]);
 
   useEffect(() => {
-    console.log("effect2");
     if (viewer.current == null) {
-        return;
+      return;
     }
     // Figure out how to avoid needing to do this every time...if possible? Why is it updating every time though?
     viewer.current.entities.removeAll();
     viewer.current.entities.suspendEvents();
     for (const entity of entities) {
-        viewer.current.entities.add(entity);
+      viewer.current.entities.add(entity);
     }
     viewer.current.entities.resumeEvents();
     viewer.current.zoomTo(viewer.current.entities);
   }, [viewer, entities]);
 
-  // How to setup the entities when they all change here.
-
-  // Entities need to be updated
-
-  return <div ref={viewerNode} className="w-scree h-screen p-0 m-0" />;
+  return <div ref={viewerNode} className="w-full h-full p-0 m-0" />;
 }
